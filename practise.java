6eaa65fb15f6634b771a8abdb89aa4adf6434d7a
Bharat N. S <74861069+bharat002@users.noreply.github.com>
2022-01-12 11:39:05 +0530

    Add files via upload

---
+++ practise.java
@@ -0,0 +1,9 @@
+class practise{
+    public static void main(String args[]){
+       int a[]={1,2,3,4,5},i;
+       for(i=0;i<a.length;i++){
+           System.out.println(a[i]);
+       } 
+      
+    }
+}
